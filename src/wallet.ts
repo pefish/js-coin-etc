6580c302e8410f45522acd75d82420daf5bca0ad
joy <joy.lai@zg.com>
2019-07-29 15:10:18 +0800

    init

---
+++ src/wallet.ts
@@ -0,0 +1,6 @@
+
+import { EthWallet } from '@pefish/js-coin-eth'
+
+export default class Wallet extends EthWallet {
+  public chainId: number = 0x3d
+}
---
+++ types/wallet.d.ts
@@ -0,0 +1,4 @@
+import { EthWallet } from '@pefish/js-coin-eth';
+export default class Wallet extends EthWallet {
+    chainId: number;
+}
